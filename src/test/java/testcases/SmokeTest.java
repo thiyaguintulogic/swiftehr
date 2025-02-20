551967ea2846faf37bbd0f83c18b3038c5c9dde9
thiyagu <thiyagarajan.c@intulogic.in>
2025-02-20 12:52:05 +0530

    new changes

--- src/test/java/testcases/SmokeTest.java
+++ src/test/java/testcases/SmokeTest.java
@@ -50,18 +50,18 @@ public class SmokeTest extends BaseTest {
 		// Fill out the registration form for the first patient
 		WebElement firstName = wait
 				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@title='First Name']")));
-		firstName.sendKeys("Camila");
+		firstName.sendKeys("Addison");
 		
 		WebElement Lastname = wait
 				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@type='text'])[2]")));
-		Lastname.sendKeys("Grayson");
+		Lastname.sendKeys("James");
 
 		WebElement age = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@title='Age']")));
-		age.sendKeys("36");
+		age.sendKeys("53");
 
 		WebElement phoneNumber = wait
 				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@title='Phone Number']")));
-		phoneNumber.sendKeys("5212321458");
+		phoneNumber.sendKeys("3212425325");
 
 		WebElement genderFemale = wait
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'Female')]")));
